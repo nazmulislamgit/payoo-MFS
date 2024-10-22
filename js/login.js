d2b4cd9c9e8de0501ff914892158ddf093b8a611
nazmulislamgit <nazmulislam.design@gmail.com>
2024-10-22 20:41:30 +0600

    update js file

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
-document.getElementById('btn-login').addEventListener('click', function(){
+// step - 1 set event handler
+document.getElementById('btn-login').addEventListener('click', function(event){
+    // step -2 prevent default behavior (prevent reloading browser)
+    event.preventDefault(); // <-- form submit 
     console.log('Login Okay');
+
+    // step - 3 get the phone number
+
+    const phoneNumber = document.getElementById('mobile-number');
+    
 })
