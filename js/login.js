fe3c9cbe922ef2b810eee24b519b598ad8bdea92
nazmulislamgit <nazmulislam.design@gmail.com>
2024-10-23 20:41:04 +0600

    Add Home Part and Js

--- js/login.js
+++ js/login.js
@@ -2,10 +2,17 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     // step -2 prevent default behavior (prevent reloading browser)
     event.preventDefault(); // <-- form submit 
-    console.log('Login Okay');
 
     // step - 3 get the phone number
 
-    const phoneNumber = document.getElementById('mobile-number');
-    
+    const mobileNumber = document.getElementById('mobile-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    // step - 4 
+    if(mobileNumber=== '828282' && pinNumber=== '1234'){
+        window.location.href = './home.html'
+    }
+    else{
+        alert('Wrong Phone Number and Pin')
+    }
 })
