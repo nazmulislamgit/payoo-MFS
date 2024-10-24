179f481a9c31b6c7fa67171bbea9dcb1a33e8641
nazmulislamgit <nazmulislam.design@gmail.com>
2024-10-24 20:59:45 +0600

    Add Money to the balance after pin verification

---
+++ js/home.js
@@ -0,0 +1,32 @@
+// add money to the account 
+
+// step-1 add an event handler to the add money button inside the form
+document.getElementById('btn-add-money')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        
+        // step-2 get money to be added to the account
+        const addMoneyInput = document.getElementById('input-add-money').value;
+        console.log(addMoneyInput);
+
+        //get the pin number provided
+        const pinNumberInput = document.getElementById('input-pin-number').value;
+        
+        // step-3 verify pin number 
+        if (pinNumberInput === '1234'){
+        // step -4 get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+
+        // step-5 add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // step -6 update the balance in the DOM
+        document.getElementById('account-balance').innerText = newBalance;
+        }
+        else{
+    alert('Failed To Add Money! Please Try again')
+        }
+    });
